3219c2f382a8e32069741aef559d7dd1095f4f51
JcBlan <blancojulic@gmail.com>
2021-03-17 17:51:52 -0300

    Estandarizado nomenclatura de tests

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -2,44 +2,44 @@ const expect = require('chai').expect;
 const Calculadora = require('../src/calculadora');
 
 let cal = new Calculadora();
-describe('Sumar', () => {
-    it(' 2 + 2 = 4', () => {  
+describe('Metodo Calculadora.Sumar', () => {
+    it(' Suma dos numeros resulta en su suma', () => {  
         expect(cal.sumar(2, 2)).to.equal(4);
     });
-    it(' 0.5 + 1 = 1.5', () => {
+    it(' Suma Entero y Decimal Resulta en Decimal', () => {
         expect(cal.sumar(0.5, 1)).to.equal(1.5);
     });
 });
 
-describe('Restar', () => {
-    it(' 5 - 3 = 2', () => {
+describe('Metodo Calculadora.Restar', () => {
+    it(' Minuendo > Sustraendo resulta en positivo', () => {
         expect(cal.restar(5, 3)).to.equal(2);
     });
-    it(' 4 - 6 = -2', () => {
+    it(' Minuendo < Sustraendo resulta en negativo', () => {
         expect(cal.restar(4, 6)).to.equal(-2);
     });
 });
 
-describe('Multiplicar', () => {
-    it(' 1 * 8 = 8', () => {
+describe('Metodo Calculadora.Multiplicar', () => {
+    it('Positivo por positivo resulta positivo', () => {
         expect(cal.multiplicar(1, 8)).to.equal(8);
     });
-    it(' -1 * 8 = -8', () => {
+    it('Positivo por negativo resulta negativo', () => {
         expect(cal.multiplicar(-1, 8)).to.equal(-8);
     });
-    it(' 1 * -8 = -8', () => {
+    it('Negativo por positivo resulta negativo', () => {
         expect(cal.multiplicar(1, -8)).to.equal(-8);
     });
-    it(' -1 * -8 = 8', () => {
+    it('Negativo por negativo resulta positivo', () => {
         expect(cal.multiplicar(-1, -8)).to.equal(8);
     });
 });
 
-describe('Dividir', () => {
-    it(' 20 / 3 = 6', () => {
+describe('Metodo Calculadora.Dividir', () => {
+    it(' Dividendo no multiplo de divisor resulta en entero', () => {
         expect(cal.dividir(20, 3)).to.equal(6);
     });
-    it(' 15 / 0 = NaN', () => {
+    it(' Division por 0 resulta en undefined', () => {
         expect(cal.dividir(15, 0)).to.equal(undefined);
     });
 });
